a21b3ca06387fa56efaf2e432707878c544fb073
jackson1404 <jackson14042004@gmail.com>
2025-06-28 22:41:22 +0630

    sec commit

--- src/main/java/com/jackson/spring_api_validation/controller/UserController.java
+++ src/main/java/com/jackson/spring_api_validation/controller/UserController.java
@@ -1,11 +1,20 @@
 package com.jackson.spring_api_validation.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jackson.spring_api_validation.dto.UserRequestDto;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
 
+    @PostMapping("/signUp")
+    public ResponseEntity<?> signUpUser(@Valid @RequestBody UserRequestDto userRequestDto){
+
+        return new ResponseEntity<>("User sign up successfully" , HttpStatus.OK);
+
+    }
 
 }
