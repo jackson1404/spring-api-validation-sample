feba46d3078ae2592ca3406334b2f1f78246a1d2
jackson1404 <jackson14042004@gmail.com>
2025-06-28 23:00:59 +0630

    added dto and exception handler

--- src/main/java/com/jackson/spring_api_validation/dto/UserRequestDto.java
+++ src/main/java/com/jackson/spring_api_validation/dto/UserRequestDto.java
@@ -1,4 +1,43 @@
 package com.jackson.spring_api_validation.dto;
 
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRequestDto {
+
+    @NotBlank(message = "Username is required.")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters.")
+    private String userName;
+
+    @NotBlank(message = "Email is required.")
+    @Email(message = "Email must be a valid email address.")
+    private String userEmail;
+
+    @Min(value = 18, message = "User must be at least 18 years old.")
+    @Max(value = 100, message = "User age cannot be more than 100.")
+    private int userAge;
+
+    @NotNull(message = "Date of birth is required.")
+    @Past(message = "Date of birth must be in the past.")
+    private LocalDate dob;
+
+    @NotBlank(message = "Password is required.")
+    @Pattern(
+            regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!*()]).{8,}$",
+            message = "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character."
+    )
+    private String userPassword;
+
+    @NotBlank(message = "Address is required.")
+    @Size(max = 255, message = "Address must not exceed 255 characters.")
+    private String userAddress;
 }
--- src/main/java/com/jackson/spring_api_validation/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jackson/spring_api_validation/exception/GlobalExceptionHandler.java
@@ -1,4 +1,37 @@
 package com.jackson.spring_api_validation.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleApiRequestValidation(MethodArgumentNotValidException exception, HttpServletRequest request){
+
+        List<String> errors = exception.getAllErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp" , LocalDate.now());
+        response.put("status" , HttpStatus.BAD_REQUEST.value());
+        response.put("path" , request.getRequestURI());
+        response.put("errors" , errors);
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+
+
+    }
+
+
 }
